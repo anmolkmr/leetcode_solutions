82a21908949d8f38687fbc8f6968316fabd3cae2
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2023-09-03 01:37:50 +0530

    Time: 42 ms (62.22%) | Memory: 21.6 MB (67.78%) - LeetSync

--- 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.cpp
+++ 2713-find-the-divisibility-array-of-a-string/find-the-divisibility-array-of-a-string.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> divisibilityArray(string word, int m) {
-        int n = word.length(),i;
-        long long int x=0;
-        vector<int> ans(n,0);
-        for(i = 0; i < n; i++){
-            x = x*10+(word[i]-'0');
-            x = x%m;
-            if(x==0){
-                ans[i] = 1;
-            }
+        int n=word.length();
+        vector<int> div(n,0);
+        long long int num=0;
+        for(int i=0;i<n;i++)
+        {
+            num=(num*10)+word[i]-'0';
+            num=num%m;
+            if(num==0)
+            div[i]=1;
         }
-        return ans;
+        return div;
     }
 };
