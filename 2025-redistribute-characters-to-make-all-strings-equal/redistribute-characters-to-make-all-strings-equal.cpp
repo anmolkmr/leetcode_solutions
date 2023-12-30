aaa7901920508f4d4021a9c6d8fbbcb109a9e23c
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2023-12-30 12:36:13 +0530

    Time: 57 ms (5.06%) | Memory: 12.8 MB (10.12%) - LeetSync

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        map<char,int> mp;
+        for(auto it:words)
+        {
+            for(auto it1:it)
+            {
+                mp[it1]++;
+            }
+        }
+        int n=words.size();
+        for(auto it:mp)
+        {
+            if(it.second%n)
+            return false;
+        }
+        return true;
+        
+    }
+};
