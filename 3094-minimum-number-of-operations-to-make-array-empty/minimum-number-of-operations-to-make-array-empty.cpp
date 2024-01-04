3b2d79465564687a9494e97b0c130c4142fb994c
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2024-01-04 18:23:30 +0530

    Time: 127 ms (41.25%) | Memory: 85.4 MB (31.49%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
+        int cnt=0;
         map<int,int> mp;
         for(auto it:nums)
-        mp[it]++;
-        int cnt=0;
-        for(auto it:mp)
-        cout<<it.first<<" "<<it.second<<endl;
+        {
+            mp[it]++;
+        }
         for(auto it:mp)
         {
             if(it.second==1)return -1;
             if(it.second%3==0)
-            cnt+=(it.second/3);
-            else if(it.second%3)
-            cnt+=(it.second/3)+1;
+            {
+                cnt+=(it.second/3);
+            }
+            else if(it.second%3==1)
+            {
+                cnt+=((it.second-3)/3)+2;
+            }
+            else if(it.second%3==2)
+            {
+                cnt+=((it.second-2)/3)+1;
+            }
             else if(it.second%2==0)
-            cnt+=(it.second/2);
+            {
+                cnt+=it.second/2;
+            }
             else
             return -1;
-
         }
         return cnt;
     }
