dbe0310a16c36ebc0819321235843c1a2889fed3
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2024-01-25 20:07:58 +0530

    Time: 39 ms (70.27%) | Memory: 25.8 MB (6.35%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int f(string &x,string &y,int i,int j,vector<vector<int>> &dp){
-        if(i < 0 || j < 0)return 0;
-        if(dp[i][j] != -1)return dp[i][j];
-        if(x[i] == y[j])
-            return dp[i][j] = 1 + f(x,y,i-1,j-1,dp);
-        return dp[i][j] =  max(f(x,y,i-1,j,dp) , f(x,y,i,j-1,dp));
+    int solve(int ind1,int ind2,string &t1,string &t2,vector<vector<int>> &dp)
+    {
+        if(ind1<0||ind2<0)return 0;
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        if(t1[ind1]==t2[ind2])
+        {
+            return 1+solve(ind1-1,ind2-1,t1,t2,dp);
+        }
+        return dp[ind1][ind2]=max(solve(ind1-1,ind2,t1,t2,dp),solve(ind1,ind2-1,t1,t2,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.length()+1,vector<int>(text2.length()+1,-1));
-       return f(text1,text2,text1.length()-1,text2.length(),dp); 
+        vector<vector<int>> dp(text1.length()+1,vector<int> (text2.length()+1,-1));
+        int ind1=text1.length()-1,ind2=text2.length()-1;
+        return solve(ind1,ind2,text1,text2,dp);
     }
 };
