624ddabcaca94c1ec1af0f69835577a111292e3f
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2024-02-26 14:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (25.40%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,19 +11,13 @@
  */
 class Solution {
 public:
-    // bool trav(TreeNode *p,TreeNode *q){
-    //     // if(p==NULL&&q!=NULL)return false;
-    //     // if(q==NULL&&p!=NULL)return false;
-    //     if(p==NULL&&q==NULL)
-    //     return p==q;
-    //     if(p->val!=q->val)return false;
-    //     trav(p->left,q->left);
-    //     trav(p->right,q->right);
-    //     return true;
-    // }
+    bool trav(TreeNode *p,TreeNode *q){
+        
+        if(p==NULL||q==NULL)
+        return p==q;
+        return (p->val==q->val)&&trav(p->left,q->left)&&trav(p->right,q->right);
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-       if(p==NULL||q==NULL)
-       return p==q;
-       return (p->val==q->val)&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+        return trav(p,q);
     }
 };
