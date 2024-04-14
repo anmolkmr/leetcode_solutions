f0153ccd5d902dd3cc6f5984167885d650f9d03d
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2024-04-14 20:18:31 +0530

    Time: 3 ms (18.25%) | Memory: 7.1 MB (67.38%) - LeetSync

--- 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool squareIsWhite(string coordinates) {
-        if((coordinates[0]-'a'+1)%2==0&&(coordinates[1]-'0')%2==0){
-            return false;
-        }
-        if((coordinates[0]-'a'+1)%2==1&&(coordinates[1]-'0')%2==1){
-            return false;
-        }
+        // if((coordinates[0]-'a'+1)%2==0&&(coordinates[1]-'0')%2==0){
+        //     return false;
+        // }
+        // if((coordinates[0]-'a'+1)%2==1&&(coordinates[1]-'0')%2==1){
+        //     return false;
+        // }
         if((coordinates[0]-'a'+1)%2==0&&(coordinates[1]-'0')%2==1){
             return true;
         }
