51a1b6ba634cb27787a1ea06311c2340019622f2
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2024-08-23 00:09:57 +0530

    Time: 50 ms (13.02%) | Memory: 24.2 MB (14.32%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    void trav(int i,int j,vector<vector<int>> &vis,int row,int col,vector<vector<char>> &grid)
-    {
+    void solve(int i,int j,vector<vector<char>> &grid,vector<vector<int>> &vis){
         vis[i][j]=1;
-        if(i+1>=0&&i+1<row&&j>=0&&j<col&&!vis[i+1][j]&&grid[i+1][j]=='1')
-        trav(i+1,j,vis,row,col,grid);
-        if(i>=0&&i<row&&j+1>=0&&j+1<col&&!vis[i][j+1]&&grid[i][j+1]=='1')
-        trav(i,j+1,vis,row,col,grid);
-        if(i>=0&&i<row&&j-1>=0&&j-1<col&&!vis[i][j-1]&&grid[i][j-1]=='1')
-        trav(i,j-1,vis,row,col,grid);
-        if(i-1>=0&&i-1<row&&j>=0&&j<col&&!vis[i-1][j]&&grid[i-1][j]=='1')
-        trav(i-1,j,vis,row,col,grid);
+        vector<int> dx={0,0,-1,1};
+        vector<int> dy={-1,1,0,0};
+
+        for(int k=0;k<4;k++){
+            int newx=i+dx[k];
+            int newy=j+dy[k];
+            if(newx>=0&&newy>=0&&newx<grid.size()&&newy<grid[0].size()&&grid[newx][newy]=='1'&&!vis[newx][newy])
+            {
+                solve(newx,newy,grid,vis);
+            }
+        }
     }
     int numIslands(vector<vector<char>>& grid) {
         int row=grid.size(),col=grid[0].size();
-        vector<vector<int>> vis(row,vector<int>(col));
-        int island=0;
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                if(!vis[i][j]&&grid[i][j]=='1')
-                {island++;
-                trav(i,j,vis,row,col,grid);
-                }
+        int cnt=0;
+        vector<vector<int>> vis(row,vector<int>(col,0));
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'&&!vis[i][j])
+                {solve(i,j,grid,vis);
+                cnt++;}
             }
         }
-        return island;
+        return cnt;
+
+
     }
 };
+
+
+
