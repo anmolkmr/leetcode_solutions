a7b1e3cf798a1682f78193d8d080d33b45078c18
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2024-09-22 02:28:19 +0530

    Time: 19 ms (6.48%) | Memory: 10.2 MB (67.21%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int count = kmp(string(s.rbegin(), s.rend()), s);
-        return string(s.rbegin(), s.rend()).substr(0, s.length() - count) + s;
-    }
+        string rev=s;
+        reverse(rev.begin(),rev.end());
 
-private:
-    int kmp(const string &txt, const string &patt) {
-        string newString = patt + '#' + txt;
-        vector<int> pi(newString.length(), 0);
-        int i = 1, k = 0;
-        while (i < newString.length()) {
-            if (newString[i] == newString[k]) {
-                k++;
-                pi[i] = k;
-                i++;
-            } else {
-                if (k > 0) {
-                    k = pi[k - 1];
-                } else {
-                    pi[i] = 0;
-                    i++;
-                }
+        for(int i=0;i<s.length();i++){
+            if(!memcmp(s.c_str(),rev.c_str()+i,s.length()-i)){
+                return rev.substr(0,i)+s;
             }
         }
-        return pi.back();
+        return rev+s;
     }
 };
