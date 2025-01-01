7b0fb5ffc7d1a0e44afc5f05c839fcbfc01cbd8b
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2025-01-01 16:03:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (23.35%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int maxScore(string s) {
-        map<char,int> mp;
-        for(auto it:s)
-        mp[it]++;
-        int zero=0,one=0;
-        if(mp['1']==0)
-        return mp['0']-1;
-        else if(mp['0']==0)
-        return mp['1']-1;
-        int mx=INT_MIN;
-        for(int i=0;i<s.length();i++)
-        {
-
+        int ans=INT_MIN,z=0,o=0;
+        for(int i=0;i<s.length();i++){
             if(s[i]=='0')
-            zero++;
-            else if(s[i]=='1')
-            one++;      
-            if(i>=0&&i<s.length()-1)
-            mx=max(mx,zero+mp['1']-one);
+            z++;
+            else
+            o++;
         }
-        return mx;
+        int zero=0;
+        for(int j=0;j<s.length()-1;j++){
+            if(s[j]=='0')
+            {
+                zero++;
+            }else {
+                o--;
+            }
+
+            ans=max(ans,zero+o);
+        }
+        return ans;
     }
 };
