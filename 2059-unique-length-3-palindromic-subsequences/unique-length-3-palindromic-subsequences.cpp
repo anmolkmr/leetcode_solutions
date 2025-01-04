39e013741d72031419362e4dd806216938f6571e
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2025-01-05 02:24:23 +0530

    Time: 371 ms (37.15%) | Memory: 16.5 MB (34.37%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<int> left(26, -1);
-        vector<int> right(26, -1);
-        vector<set<char>> pms(26);
-        for(int i = 0; i < s.length(); i++){
-            int idx = s[i] - 'a';
-            if(left[idx] == -1) left[idx] = i;
-        }
-        for(int i = s.length() - 1; i >= 0; i--){
-            int idx = s[i] - 'a';
-            if(right[idx] == -1) right[idx] = i;
-        }
-        for(int i=0; i < s.length(); i++)
-        {
-            for (int j = 0; j < 26; j++){
-                if(left[j] < i && i < right[j]) pms[j].insert(s[i]);
+        vector<int> right(26,-1),left(26,-1);
+        set<char> arr;
+        for(int i=0;i<s.length();i++){
+            arr.insert(s[i]);
+            int idx=s[i]-'a';
+            if(right[idx]==-1){
+                right[idx]=i;
             }
-            
         }
-        int count = 0;
-        for(int i=0; i < 26; i++)
-        {
-            count += pms[i].size();
+        for(int i=s.length()-1;i>=0;i--){
+            int idx=s[i]-'a';
+            if(left[idx]==-1){
+                left[idx]=i;
+            }
         }
-        return count;
+        int cnt=0;
+        for(auto it:arr){
+            int l=left[it-'a'];
+            int r=right[it-'a'];
+            cout<<r<<l;
+            set<char> temp;
+            for(int i=r+1;i<l;i++){
+                temp.insert(s[i]);
+            }
+            cnt+=temp.size();
+        }
+        return cnt;
     }
 };
