4d92dc5971a99724b8881061cd87ae7a66eae120
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2025-02-16 23:36:35 +0530

    Time: 3 ms (19.91%) | Memory: 8.6 MB (36.80%) - LeetSync

--- 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
-    bool solve(int ind,int n,vector<int> &used,vector<int> &res){
-        if(ind>=res.size())return true;
-        if(res[ind] != -1) {
-            return solve(ind+1, n, used, res);
+    bool solve(int ind,int n,vector<int> &res,vector<bool> &used){
+        if(ind>=res.size())
+        return true;
+        if(res[ind]!=-1)
+        {
+            return solve(ind+1,n,res,used);
         }
         for(int i=n;i>=1;i--){
-            if(used[i]!=-1)continue;
-            used[i]=i;
+            if(used[i]){
+                continue;
+            }
+            used[i]=true;
             res[ind]=i;
             if(i==1){
-                if(solve(ind+1,n,used,res))
+                if(solve(ind+1,n,res,used))
                 return true;
             }
             else{
                 int k=ind+res[ind];
-                if(k<res.size()&&res[k]==-1)
-                {
+                if(k<res.size()&&res[k]==-1){
                     res[k]=i;
-                    if(solve(ind+1,n,used,res))
+                    if(solve(ind+1,n,res,used))
                     return true;
+
                     res[k]=-1;
                 }
             }
-            used[i]=-1;
+            used[i]=false;
             res[ind]=-1;
+
         }
         return false;
     }
     vector<int> constructDistancedSequence(int n) {
-        vector<int> used(n+1,-1),res(2*n-1,-1);
-        solve(0,n,used,res);
+        vector<int> res(2*n-1,-1);
+        vector<bool> used(n+1,false);
+        solve(0,n,res,used);
         return res;
     }
 };
