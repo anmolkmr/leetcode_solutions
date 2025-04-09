c7a16370360f67a153e7b50b8359dc8c6e9629a2
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2025-04-10 02:30:39 +0530

    Time: 11 ms (63.84%) | Memory: 31.1 MB (97.80%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
+    int secondLargest(vector<int> &nums){
+       sort(nums.begin(),nums.end());
+       int n=nums.size()-1,lastIndex=nums[n];
+       for(int i=n;i>=0;i--){
+        if(nums[i]!=lastIndex)
+        return nums[i];
+       }
+       return -1;
+    }
     int minOperations(vector<int>& nums, int k) {
-        map<int,int> mp;
-        int n=INT_MAX;
-        for(auto it:nums){
-            mp[it]++;
-            n=min(n,it);
+        int min = *min_element(nums.begin(),nums.end());
+        if(min<k)return -1;
+        int sl=0,count=0;;
+        while(true){
+            
+            sl=secondLargest(nums);
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]>sl)
+                nums[i]=sl;
+            }
+            if(sl==-1&&k==min)
+            return count;
+            else if(sl==-1)
+            return count+1;
+            
+            count++;
+    
         }
-        if(k>n)
-        return -1;
-        if(n==k)
-        return mp.size()-1;
-        return mp.size();
-
+        return count;
     }
 };
