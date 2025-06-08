ce0dda8635b9f9f809cf45b16845d2fed0dadc75
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2025-06-08 16:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (52.72%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    void solve(int c,int n,vector<int> &res){
-        if(c>n)return ;
-        res.push_back(c);
-        for(int app=0;app<10;app++){
-            int temp=(c*10)+app;
-            if(temp>n)return ;
-            solve(temp,n,res);
+    void lex(int s,int n,vector<int> &ans){
+        if(s>n)return ;
+        ans.push_back(s);
+        for(int i=0;i<=9;i++){
+            int k=(s*10)+i;
+            lex(k,n,ans);
         }
     }
     vector<int> lexicalOrder(int n) {
-        vector<int> res;
+        vector<int> ans;
         for(int i=1;i<=9;i++){
-            solve(i,n,res);
+            // ans.push_back(i);
+            lex(i,n,ans);
         }
-        return res;
+        return ans;
     }
 };
