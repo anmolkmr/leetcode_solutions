5e1c9b5f60db445c422f01860778598dddb9a1d6
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2025-11-04 00:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.71%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        if(digits[digits.size()-1]<9){
+            digits[digits.size()-1]++;
+            return digits;
+        }
+        else{
+            int i=digits.size()-1;
+            while(i>=0&&digits[i]==9){
+                digits[i]=0;
+                i--;
+            }
+            if(i<0){
+                digits.insert(digits.begin()+0,1);
+                return digits;
+            }
+            digits[i]++;
+            return digits;
+        }
+    }
+};
