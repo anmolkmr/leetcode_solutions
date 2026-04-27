26b2a19e0d8a9f8ffac4a58b04ab7432f85c8c79
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-04-28 00:49:03 +0530

    Time: 3 ms (37.29%) | Memory: 15.5 MB (11.39%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    void solve(int ind,vector<int> &ds,int target,vector<int>& candidates,vector<vector<int>> &ans){
-       // if(ind>=candidates.size())return ;
-        // if(target==0)
-        // {
-        //     ans.push_back(ds);
+    void solve(int ind,vector<int> ds,vector<vector<int>> &ans,int tar,vector<int>& can){
+        // if(ind>=can.size()){
+        //     if(tar==0){
+        //         ans.push_back(ds);
+                
+        //     }
         //     return ;
         // }
-        if(target==0||ind==candidates.size())
-        {
-            if(target==0)
+
+        if(tar==0){
             ans.push_back(ds);
             return;
         }
-        
-       for(int i=ind;i<candidates.size();i++){
-        if(i>ind&&candidates[i]==candidates[i-1])continue;
-        if(candidates[i]>target)break;
-        ds.push_back(candidates[i]);
-        solve(i+1,ds,target-candidates[i],candidates,ans);
-        ds.pop_back();
-       }
+
+        for(int i=ind;i<can.size();i++){
+            if(i>ind&&can[i-1]==can[i])continue;
+            if(tar<can[i])break;
+            ds.push_back(can[i]);
+            solve(i+1,ds,ans,tar-can[i],can);
+            ds.pop_back();
+        }
+
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
         vector<vector<int>> ans;
         vector<int> ds;
-        solve(0,ds,target,candidates,ans);
+        sort(candidates.begin(),candidates.end());
+        solve(0,ds,ans,target,candidates);
         return ans;
     }
 };
