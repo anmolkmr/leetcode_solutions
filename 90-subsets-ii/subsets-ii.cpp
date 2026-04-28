fdb52bd1dcc2278af53d1852856af47ff229d9fe
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-04-29 02:16:07 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (20.89%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    void solve(int ind,vector<int> ds,vector<int> &nums,set<vector<int>> &ans){
-        if(ind>=nums.size()){
-            ans.insert(ds);
-            return;
-        }
-        ds.push_back(nums[ind]);
-        solve(ind+1,ds,nums,ans);
-        ds.pop_back();
-        solve(ind+1,ds,nums,ans);
-        // for(int i=ind;i<nums.size();i++){
-        //     if(i>ind&&nums[i-1]==nums[i])continue;
-        //     ds.push_back(nums[ind]);
-        //     solve(i+1,ds,nums,ans);
-        //     ds.pop_back();
+    void solve(int ind,vector<int> ds,vector<int> &nums,vector<vector<int>> &ans){
+        // if(ind>=nums.size()){
+        //     ans.insert(ds);
+        //     return;
         // }
+        // ds.push_back(nums[ind]);
+        // solve(ind+1,ds,nums,ans);
+        // ds.pop_back();
+        // solve(ind+1,ds,nums,ans);
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++){
+            if(i!=ind&&nums[i-1]==nums[i])continue;
+            ds.push_back(nums[i]);
+            solve(i+1,ds,nums,ans);
+            ds.pop_back();
+        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<vector<int>> ans2;
         vector<int> ds;
         sort(nums.begin(),nums.end());
         solve(0,ds,nums,ans);
-        for(auto it:ans)
-        ans2.push_back(it);
-        return ans2;
+        // for(auto it:ans)
+        // ans2.push_back(it);
+        return ans;
     }
 };
