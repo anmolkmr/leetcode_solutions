a4371d476ea31d6b0960ad380756d4a5a03c0a9d
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-04-28 14:55:46 +0530

    Time: 124 ms (6.17%) | Memory: 102.8 MB (5.35%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int eq=grid[0][0]%x;
-        vector<int> vs;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                vs.push_back(grid[i][j]);
-                if(grid[i][j]%x!=eq)
+        vector<int> arr;
+        int maxi=INT_MIN;
+        map<int,int> mp;
+        for(auto it:grid)
+        {
+            for(auto it1:it){
+                maxi=max(maxi,it1);
+                arr.push_back(it1);
+                mp[it1]++;
+            }
+        }
+
+        sort(arr.begin(),arr.end());
+
+        for(auto it:arr){
+            if((maxi-it)%x){
                 return -1;
             }
         }
-        sort(vs.begin(),vs.end());
-        int mid=vs.size()/2;
-        int cnt=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                int l=abs(grid[i][j]-vs[mid]);
-                cnt+=l/x;
-            }
+        int maximap=INT_MIN,no;
+        int k=arr.size()/2;
+        no=arr[k];
+        // for(auto it:mp){
+        //     if(maximap<it.second){
+        //         maximap=it.second;
+        //         no=it.first;
+        //     }
+        // }
+        int steps=0;
+
+        for(auto it:arr){
+            steps+=(abs(no-it)/x);
         }
-        return cnt;
+        return steps;
+
+
 
     }
 };
