ab0ff48971b552d9e31a50c5a5fd3b2dfc3ab707
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-04-29 02:48:42 +0530

    Time: 3 ms (27.08%) | Memory: 12.2 MB (9.78%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-    void solve(vector<int> &ds,vector<int> &freq,vector<int>& nums,vector<vector<int>> &ans)
-    {
-        if(ds.size()==nums.size())
-        {
+    void solve(int ind,vector<int> ds,vector<int> &vis,vector<vector<int>> &ans,vector<int> &nums){
+        if(ds.size()==nums.size()){
             ans.push_back(ds);
-            return;
+            return ;
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(!freq[i])
-            {
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                solve(ds,freq,nums,ans);
-                ds.pop_back();
-                freq[i]=0;
-            }
+        for(int i=0;i<vis.size();i++){
+            if(vis[i]==0){
+            vis[i]=1;
+            ds.push_back(nums[i]);
+            solve(i+1,ds,vis,ans,nums);
+            ds.pop_back();
+            vis[i]=0;}
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int n=nums.size();
         vector<vector<int>> ans;
+        vector<int> vis(nums.size());
         vector<int> ds;
-        vector<int> freq(n);
-        solve(ds,freq,nums,ans);
-        return ans; 
+        solve(0,ds,vis,ans,nums);
+        return ans;
     }
 };
