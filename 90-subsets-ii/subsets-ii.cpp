5c09af25f125dae6fe6b8abf1bea326b0e5a9735
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-04-29 01:19:19 +0530

    Time: 7 ms (6.61%) | Memory: 17.6 MB (5.33%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    void solve(int ind,vector<int> &nums,vector<int> &ds,set<vector<int>> &ans){
+    void solve(int ind,vector<int> ds,vector<int> &nums,set<vector<int>> &ans){
         if(ind>=nums.size()){
             ans.insert(ds);
-            return ;
+            return;
         }
         ds.push_back(nums[ind]);
-        solve(ind+1,nums,ds,ans);
+        solve(ind+1,ds,nums,ans);
         ds.pop_back();
-        solve(ind+1,nums,ds,ans);
+        solve(ind+1,ds,nums,ans);
+        // for(int i=ind;i<nums.size();i++){
+        //     if(i>ind&&nums[i-1]==nums[i])continue;
+        //     ds.push_back(nums[ind]);
+        //     solve(i+1,ds,nums,ans);
+        //     ds.pop_back();
+        // }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         set<vector<int>> ans;
+        vector<vector<int>> ans2;
         vector<int> ds;
         sort(nums.begin(),nums.end());
-        solve(0,nums,ds,ans);
-        vector<vector<int>> ans2;
-        for(auto it:ans){
-            ans2.push_back(it);
-        }
+        solve(0,ds,nums,ans);
+        for(auto it:ans)
+        ans2.push_back(it);
         return ans2;
     }
 };
