608a63dbd1a812ab8dbc52c54a637a13e5db1461
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-06-11 12:48:25 +0530

    Time: 46 ms (24.25%) | Memory: 37.1 MB (25.69%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int solve(int ind,int amt,vector<int> &coins,vector<vector<int>> &dp)
-    {
-        if(amt==0)return 1;
-        if(ind<=0)
-        {
-            if(amt%coins[ind]==0)
-            return 1;
+    int solve(int ind,int amt,vector<int> &coins,vector<vector<int>> &dp){
+        if(ind==coins.size()){
+            if(amt==0)return 1;
             return 0;
         }
+        if(amt==0)return 1;
         if(dp[ind][amt]!=-1)return dp[ind][amt];
-        if(amt<0)return 0;
-        int notpick=solve(ind-1,amt,coins,dp);
-        int pick=0;
-        if(amt>=coins[ind])
-        {
+        int pick=0,notpick=0;
+        if(coins[ind]<=amt){
             pick=solve(ind,amt-coins[ind],coins,dp);
         }
+        notpick=solve(ind+1,amt,coins,dp);
         return dp[ind][amt]=pick+notpick;
-    }
+        }
+    
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(n-1,amount,coins,dp);
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
+        return solve(0,amount,coins,dp);
     }
 };
