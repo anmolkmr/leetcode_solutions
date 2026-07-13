202978545cea7f568ddb0e87e21b8a9af586eb3f
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-07-13 19:13:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (8.31%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int countdigit(int low) {
-        int cnt=0;
-        while(low) {
-            cnt++;
-            low=low/10;
+    int countDigits(int num){
+        int k=0;
+        while(num){
+            num=num/10;
+            k++;
         }
-        return cnt;
-    }
-    void generate(int dig,vector<int> &ans,int low,int high) {//naya ans sab aajayega
-        for(int i=1;i<=(10-dig);i++) {
-            int generatednumber=0;
-            for(int j=i;j<i+dig;j++) {
-                generatednumber=(generatednumber*10)+j;
-            }
-            if(generatednumber>=low&&generatednumber<=high)
-            ans.push_back(generatednumber);
-        }
+        return k;
     }
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> ans;
-        int n = countdigit(high);
-        for(int i = countdigit(low);i <= n ; i++) {
-            generate(i,ans,low,high);
+        int lowDigit=countDigits(low),highDigit=countDigits(high);
+        set<int> ans;
+        int diff=highDigit-lowDigit;
+        for(int x=0;x<=diff;x++){
+            for(int y=1;y<=9;y++){
+                int dig=0;
+                string s=to_string(y);
+                char c=s[0];
+                string l="";
+                for(int z=0;z<lowDigit+x;z++){
+                    l=l+char(c+z);
+                }
+                if(low<=stoi(l)&&stoi(l)<=high)
+                ans.insert(stoi(l));
+            }
         }
-        return ans;
-        //99 3000 2 4
-        //12 23 234 345 456 567 678 789 1234 2345 3456 4567 5678 6789
-        //123 234 345 456 567 678 789 1234 2345 
-        // dig 2345%10
-        // rev=rev*10+dig
-        
+        vector<int> ans2;
+        for(auto it:ans)
+        ans2.push_back(it);
+        return ans2;
     }
 };
