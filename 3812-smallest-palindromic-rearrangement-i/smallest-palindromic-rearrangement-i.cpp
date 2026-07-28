66fd223251a95deeb98fd7aec019983d7a72d8a4
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-07-29 00:59:09 +0530

    Time: 48 ms (50.11%) | Memory: 77.9 MB (21.98%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -15,27 +15,15 @@ public:
             }
         }
 
-        // cout<<uniq<<endl;
-
-        vector<char> ans(s.length(),'a');
+        vector<char> ans(s.length());
 
         int first=0,last=s.length()-1;
-
-        // for(auto it:mp){
-        //     cout<<it;
-        // }
-
-        // cout<<"as";
-        string s1="",s2="";
-
-
         for(int i=0;i<26;i++){
             for(int j=0;j<mp[i];j+=2){
                 
                 char temp=char(97+i);
                 ans[first]=temp;
                 ans[last]=temp;
-                s1+=temp;
                 first++;
                 last--;
             }
@@ -43,9 +31,6 @@ public:
         if(s.length()%2){
             ans[s.length()/2]=uniq;
         }
-
-        // for(auto it:ans)
-        // cout<<it;
 
         string ans2="";
         for(auto it:ans){
