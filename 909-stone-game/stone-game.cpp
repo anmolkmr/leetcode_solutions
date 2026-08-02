f9c7a26dddb95415f9d517e8e3dc308d8d18ece2
anmolkmr <69435247+anmolkmr@users.noreply.github.com>
2026-08-02 19:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.05%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<int> &piles,vector<vector<int>> &dp){
-        if(i>j)return 0;
-        if(i==j)return piles[i];
-        if(dp[i][j]!=-1)return dp[i][j];
-        int pickLeft=piles[i]+min(solve(i+2,j,piles,dp),solve(i+1,j-1,piles,dp));
-        int pickRight=piles[j]+min(solve(i+1,j-1,piles,dp),solve(i,j-2,piles,dp));
-        return dp[i][j]=max(pickRight,pickLeft);
-    }
     bool stoneGame(vector<int>& piles) {
-        vector<vector<int>> dp(piles.size()+1,vector<int>(piles.size()+1,-1));
-        int p1=solve(0,piles.size()-1,piles,dp),total=0;
-        for(auto it:piles)total+=it;
-        if(p1>total-p1)return true;
-        return false;
-        
+        return true;
     }
 };
